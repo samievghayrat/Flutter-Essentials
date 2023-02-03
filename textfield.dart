a2628bcff3f463080dd9980aa65069905599e46e
Ghayrat Samiev <81125049+samievghayrat@users.noreply.github.com>
2023-02-04 01:29:49 +0900

    Create textfield.dart

---
+++ textfield.dart
@@ -0,0 +1,22 @@
+TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                focusedBorder: new UnderlineInputBorder(
+                    borderSide: new BorderSide(
+                        color: Colors.green,
+                        width: 2,
+                        style: BorderStyle.solid)),
+                labelText: "Product Price",
+                icon: Icon(
+                  Icons.assignment,
+                  color: Colors.orangeAccent,
+                ),
+                fillColor: Colors.white,
+                labelStyle: TextStyle(
+                  color: Colors.green,
+                )),
+            onChanged: (value) {
+              productPrice = value;
+            },
+            controller: productPriceController,
+          ),
